e823357b25539d18ea663de4b9bb366c5d26b7d4
Shain-H <hodzic.shain@gmail.com>
2022-03-28 17:01:09 -0400

    +Dictionary.py +Average.html

---
+++ dictionary.v02.py
@@ -0,0 +1,17 @@
+myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+
+myKeys = [] #this is an empty array
+myValues = []
+
+# parallel iteration
+
+for k, v in myData.items():
+  #k and v are two incremental variables iterating simultaneously
+  #.items() is a method that allows us to iterate as many times as there are "ITEMS" inside our dictionary
+  print("key: " + str(k) +  ", value: " + str(v)) #monitor status of each variable on each iteration!
+  myKeys.append(k)
+  myValues.append(v)
+  # .append() is another method that adds items to an Array or list
+
+print("ALL KEYS: " + str(myKeys))
+print("ALL VALUES: " + str(myValues))
--- party.py
+++ party.py
@@ -4,6 +4,6 @@ partyCountDown = int(partyCountDown)
 if partyCountDown < 1:
     print("PARTY NOW!!")
 else:
-    for i in range(partyCountDown, 0, -1)
-    if i == 1:
-        print("PARTY TIME!!")
+    for i in range(partyCountDown, 0, -1):
+         if i == 1:
+             print("PARTY TIME!!")
